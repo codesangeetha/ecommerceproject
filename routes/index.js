f0174856294766c774a234222d53ad00f9098114
sangeetha <sangeetharenganath98@gmail.com>
2024-11-04 00:03:51 +0530

    login page designed

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+  return res.render('index');
 });
 
-router.get('/product',(req,res)=>{
- res.render('product');
+router.get('/product', (req, res) => {
+  return res.render('product');
 });
-router.get('/cart',(req,res)=>{
-  res.render('cart');
- });
- router.get('/login',(req,res)=>{
-  res.render('login');
- });
- router.get('/signup',(req,res)=>{
-  res.render('signup');
- });
- router.get('/checkout',(req,res)=>{
-  res.render('checkout');
- });
- router.get('/payment',(req,res)=>{
-  res.render('payment');
- });
- 
+router.get('/cart', (req, res) => {
+  return res.render('cart');
+});
+
+router.get('/login', (req, res) => {
+  return res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  return res.render('signup');
+});
+router.get('/checkout', (req, res) => {
+  return res.render('checkout');
+});
+router.get('/payment', (req, res) => {
+  return res.render('payment');
+});
+
 module.exports = router;
